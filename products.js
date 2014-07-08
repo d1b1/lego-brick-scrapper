b1cef2c753611c3ac8421c4b87c7052b1274db3c
Stephan Smith <Stephan.Smith.BC93@gmail.com>
2014-07-08 09:15:25 -0400

    Updated the product importer.

--- products.js
+++ products.js
@@ -6,15 +6,29 @@ var _       = require('underscore');
 
 // This works for Star Wars Only.
 var url = 'http://service.lego.com/Views/Service/Pages/BIService.ashx/GetThemeListHtml?searchValue=10000-20056&fromIdx='
-
+var sUrl = 'http://api.legojs.io/product/search?'
 var allData = [];
 var totalCounter = 0;
+var t = 0;
 
 var n = async.queue(function (json, callback) {
-  request.post('http://api.legojs.io/product', callback).form(json);
+
+  request.get({ url: sUrl + 'id=' + json.ProductId, json:true }, function(err, res) {
+
+    if (res.body.count == 0) {
+      totalCounter++;
+      request.post('http://api.legojs.io/product', callback).form(json);
+    } else {
+      var Ujson = res.body.results[0];
+      Ujson.pdf_Url.push(json.pdf_Url);
+
+      request.put('http://api.legojs.io/product', callback).form(Ujson);
+    }
+  });
+
 }, 20);
 
-n.drain = function() {
+n.drain = function() {;
   console.log('Finished Product Inserts to the API.');
 }
 
@@ -26,9 +40,9 @@ var q = async.queue(function (page, callback) {
         productId: el.ProductId,
         name:      el.ProductName,
         image:     el.ImageLocation,
-        pdf_Url:   el.PdfLocation
+        pdf_Url:   [ el.PdfLocation ]
       };
-      totalCounter++;
+
       n.push(json, function (err) {});
     });
 
@@ -37,6 +51,7 @@ var q = async.queue(function (page, callback) {
 }, 20);
 
 q.drain = function() {
+  console.log('total', t)
   console.log('Finished Paging.');
   console.log('Total Products ' + totalCounter);
 }
@@ -44,11 +59,12 @@ q.drain = function() {
 request.get(url + '0', function(err, res) {
   var body = res.body;
   var data = JSON.parse(body);
-  var pages = data.Count;
+  var pages = parseFloat(data.Count);
 
-  for (i = 0; i < pages-1; i++) {
-    console.log('Page: ' + i);
+  var i = 0;
+  while (i < pages ) {
     q.push(i, function (err) {});
-  }
+    i = i + 10;
+}
 
 });
